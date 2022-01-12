c5b183e45e2fbe0a1ae5c042ab11c6eb73e94111
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-01-11 22:17:27 -0300

    nacionalidade
    condicional simples

---
+++ JavaScript/folha.js
@@ -0,0 +1,13 @@
+    var txtn = window.document.getElementById('txtnas')
+    var res = window.document.getElementById('res')
+    var nas = Number('txtn.value')
+    
+function calcular() {
+    if(nas == 1){
+        res.innerHTML += "<p>É muito legal que você nasceu no <strong>Brasil</strong>, Estão você é brasileiro</P>"
+    } 
+        res.innerHTML +="<p>Felicidades Pra você!</p>"
+    
+    
+
+}
